13f800586cfda5d61bb69d43bf59094293e56d4c
appvin <appvin@outlook.com>
2017-11-07 23:23:18 +0800

    create

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-
-        String[] strings=new String[]{"hot","dot","dog","lot","log","cog"};
-        List<String> list= Arrays.asList(strings);
-        System.out.println(new Solution().ladderLength("hit","cog",list));
+    public static void main(String[] args) {
+        LRUCache cache=new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        cache.get(2);       // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        cache.get(1);       // returns -1 (not found)
+        cache.get(3);       // returns 3
+        cache.get(4);       // returns 4
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
+    public ListNode sortList(ListNode head) {
 
+    }
+
+    private ListNode sort(ListNode left,ListNode right,int n){
+        ListNode cur=new ListNode(0);
+        ListNode head=cur;
+        int i=0,j=0;
+        while (i<n||j<n){
+            if (i==n){
+                cur.next=right;
+                right=right.next;
+                left=left.next;
+                j++;
+            }
+            else if (j==n){
+                cur.next=left;
+                i++;
+            }
+        }
+    }
 }
